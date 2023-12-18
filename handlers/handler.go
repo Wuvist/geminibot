63ba3aafeebd4b25cc396d15ccfe409a5a8ea387
Weng Wei <wuvist@gmail.com>
2023-12-18 14:52:20 +0800

    Remove config

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"log"
 
-	"github.com/Wuvist/geminibot/config"
 	"github.com/eatmoreapple/openwechat"
 )
 
@@ -38,16 +37,16 @@ func Handler(msg *openwechat.Message) {
 		return
 	}
 
-	// 好友申请
-	if msg.IsFriendAdd() {
-		if config.LoadConfig().AutoPass {
-			_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
-			if err != nil {
-				log.Fatalf("add friend agree error : %v", err)
-				return
-			}
-		}
-	}
+	// // 好友申请
+	// if msg.IsFriendAdd() {
+	// 	if config.LoadConfig().AutoPass {
+	// 		_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
+	// 		if err != nil {
+	// 			log.Fatalf("add friend agree error : %v", err)
+	// 			return
+	// 		}
+	// 	}
+	// }
 
 	// 私聊
 	handlers[UserHandler].handle(msg)
