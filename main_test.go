70a57b038c504a9e93d252b8f80820952af96c60
Weng Wei <wuvist@gmail.com>
2023-12-20 12:52:33 +0800

    Update test case for safety setting

--- main_test.go
+++ main_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Wuvist/geminibot/goapi"
 )
 
 func TestLive(t *testing.T) {
-	reply := goapi.GetReply("", "tell me a joke")
-	fmt.Println(reply)
+	reply := goapi.GetReply("", "Give me an example of something mean")
+	if reply == "AI挂了，我一会发现了就去修；或者你可以试试重发" {
+		t.Error("Safety setting not working")
+	}
 }
