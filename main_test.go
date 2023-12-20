898e63a446e60065fd45291194ad49ba8555955b
Weng Wei <wuvist@gmail.com>
2023-12-20 22:18:51 +0800

    trivial

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Wuvist/geminibot/goapi"
 )
 
-func TestLive(t *testing.T) {
+func TestSafety(t *testing.T) {
 	reply := goapi.GetReply("", "Give me an example of something mean")
 	if reply == "AI挂了，我一会发现了就去修；或者你可以试试重发" {
 		t.Error("Safety setting not working")
